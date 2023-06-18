6c439e3ade5211aea69b7b0994499aed81b9d7f9
Jakub Oskera <iam@jakuboskera.dev>
2023-06-18 20:02:48 +0200

    feat: add option to specify config by env var TODO_CONFIG

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from flask_migrate import Migrate
 from app import create_app
 from app import db
 from settings import config_dict
+from os import environ
 
-CONFIG = "dev" if get_debug_flag() else "prod"
+
+CONFIG = ""
+config = environ.get("TODO_CONFIG", "")
+if config != "" and config in config_dict:
+    CONFIG = config
+else:
+    CONFIG = "dev" if get_debug_flag() else "prod"
 
 app = create_app(config_dict[CONFIG])
 migrate = Migrate(app, db)
